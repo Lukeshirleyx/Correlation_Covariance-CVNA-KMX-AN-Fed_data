4e3c6ec32df0cfaa7a092f2bcc1e9d1103afaa82
Luke Shirley <Lukeshirley3@gmail.com>
2019-07-24 18:01:57 -0500

    Update CVNA-KMX-AN-Fed_data.py

--- CVNA-KMX-AN-Fed_data.py
+++ CVNA-KMX-AN-Fed_data.py
@@ -5,4 +5,10 @@ import yfinance as yf
 stocks = 'CVNA KMX AN'
 stocks = stocks.split()
 data = yf.download(stocks, '2017-04-28')['Close']
-data.head()
+data.head(600)
+
+import matplotlib.pyplot as plt
+%matplotlib inline
+plt.figure(figsize=(17,10))
+plt.plot(data)
+plt.legend(['AN', 'CVNA', 'KMX'], loc=2)
